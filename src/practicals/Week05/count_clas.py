7f340e78de5df7e2fbfebbd8eed0999baad8f91d
Tony Jenkins <A.M.Jenkins@leedsbeckett.ac.uk>
2024-02-27 15:45:49 +0000

    Improve grammar in CLA counter

--- src/practicals/Week05/count_clas.py
+++ src/practicals/Week05/count_clas.py
@@ -6,4 +6,6 @@ import sys
 
 if __name__ == '__main__':
 
-    print(len(sys.argv) - 1)
+    clas = len(sys.argv) - 1
+    print(f'There {"was" if clas == 1 else "were"} {clas} '
+          f'argument{"" if clas == 1 else "s"} on the line.')
